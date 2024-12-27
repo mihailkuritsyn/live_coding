8aee103b4a9d6d398df5e23c5ef8a299b54c888e
VTB4056382@region.vtb.ru <mvkuricyn@vtb.ru>
2024-12-27 19:55:36 +0300

    CoinChange322

--- src/main/java/leetcode/medium/dp/CoinChange322.kt
+++ src/main/java/leetcode/medium/dp/CoinChange322.kt
@@ -1,6 +1,7 @@
 package leetcode.medium.dp
 
-//You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+//You are given an integer array coins representing coins of
+// different denominations and an integer amount representing a total amount of money.
 //
 //Return the fewest number of coins that you need to make up that amount.
 // If that amount of money cannot be made up by any combination of the coins,
@@ -10,7 +11,57 @@ package leetcode.medium.dp
 
 class CoinChange322 {
 
+    //------- bottom to top dp solution-----
+    fun coinChange(coins: IntArray, amount: Int): Int {
+        if (amount == 0) return 0
+        val dp = IntArray(amount + 1) { Int.MAX_VALUE }
+        dp[0] = 0
+        for (i in 1..amount) {
+            for (j in coins.indices) {
+                val rem = i - coins[j]
+                if (rem >= 0 && dp[rem] != Int.MAX_VALUE) {
+                    dp[i] = minOf(dp[i], 1 + dp[rem])
+                }
+            }
+        }
+        return if (dp[amount] == Int.MAX_VALUE) -1 else dp[amount]
+    }
+
+    //------- top to bottom brute force solution-----
+//    private val memo = mutableMapOf<Int, Int>()
+//    var res = Int.MAX_VALUE
+//    lateinit var coins_: IntArray
+//
 //    fun coinChange(coins: IntArray, amount: Int): Int {
+//        if (amount == 0) return 0
+//        coins_ = coins
+//        res = change(amount)
+//        return if (res == Int.MAX_VALUE) -1 else res
+//    }
+//
+//    fun change(amount: Int): Int {
+//        if (amount < 0) {
+//            return Int.MAX_VALUE
+//        }
+//        if (amount == 0) {
+//            return 0
+//        }
+//        if (memo.containsKey(amount)) {
+//            return memo[amount]!!
+//        }
+//        var currMin = Int.MAX_VALUE
+//        for (i in coins_.indices) {
+//            val change = change(amount - coins_[i])
+//            if (change != Int.MAX_VALUE) {
+//                currMin = minOf(currMin, 1 + change)
+//            }
+//        }
+//        memo[amount] = currMin
+//        return currMin
+//    }
+
+//    -----------greedy algo (fail)---------
+    //    fun coinChange(coins: IntArray, amount: Int): Int {
 //        if (amount == 0) return 0
 //        coins.sortDescending()
 //        for (i in coins.indices) {
@@ -29,36 +80,4 @@ class CoinChange322 {
 //        }
 //        return -1
 //    }
-
-
-    //------- time limit exceeded solution-----
-    private val memo = mutableMapOf<Int, Int>()
-    var res = Int.MAX_VALUE
-    lateinit var coins_: IntArray
-
-    fun coinChange(coins: IntArray, amount: Int): Int {
-        if (amount == 0) return 0
-        coins_ = coins
-        res = change(0, amount)
-        return if (res == Int.MAX_VALUE) -1 else res
-    }
-
-    fun change(counter: Int, amount: Int): Int {
-        if (amount < 0) {
-            return Int.MAX_VALUE
-        }
-        if (amount == 0) {
-            return counter
-        }
-        if (memo.containsKey(amount)) {
-            return memo[amount]!!
-        }
-        var currMin = Int.MAX_VALUE
-        for (i in coins_.indices) {
-            val change = change(counter + 1, amount - coins_[i])
-            currMin = minOf(currMin, change)
-        }
-        memo[amount] = currMin
-        return currMin
-    }
 }
